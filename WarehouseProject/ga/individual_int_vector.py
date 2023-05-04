5b9866baea47b81ab4e4fc81ee91cb29a14aba33
Imeguras <imeguras@teknik.io>
2023-05-04 11:24:29 +0100

    #retodo

--- WarehouseProject/ga/individual_int_vector.py
+++ WarehouseProject/ga/individual_int_vector.py
@@ -7,7 +7,7 @@ class IntVectorIndividual(Individual):
 
     def __init__(self, problem: Problem, num_genes: int):
         super().__init__(problem, num_genes)
-        # TODO
+        # RETODO
 
     def swap_genes(self, other, index: int):
         aux = self.genome[index]
