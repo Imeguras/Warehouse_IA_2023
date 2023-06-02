8f490a1335b9b6558816e517cbd45d34a02216b6
Imeguras <imeguras@teknik.io>
2023-06-02 16:30:03 +0100

    Old Changes

--- WarehouseProject/warehouse/pair.py
+++ WarehouseProject/warehouse/pair.py
@@ -10,5 +10,5 @@ class Pair:
             self.cell2.line) + "_" + str(self.cell2.column)
 
     def __str__(self):
-        return str(self.cell1.line) + "-" + str(self.cell1.column) + " / " + str(self.cell2.line) + "-" + str(self.cell2.column) + ": " + str(format(self.value,".4f")) + "\n"
+        return str(self.cell1.line) + "-" + str(self.cell1.column) + " / " + str(self.cell2.line) + "-" + str(self.cell2.column) + ": " + str(self.value) + "\n"
 
