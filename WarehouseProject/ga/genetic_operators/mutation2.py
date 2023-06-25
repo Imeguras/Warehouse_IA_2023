af7b98bc348d5a060a351b205a2ed2a798c8cb53
Ciborg085 <1903bernardo@gmail.com>
2023-06-25 17:48:59 +0100

    Implementado invert mutation

--- WarehouseProject/ga/genetic_operators/mutation2.py
+++ WarehouseProject/ga/genetic_operators/mutation2.py
@@ -1,13 +1,21 @@
 from ga.individual_int_vector import IntVectorIndividual
 from ga.genetic_operators.mutation import Mutation
+from ga.genetic_algorithm import GeneticAlgorithm
 
+# Swap mutation
 class Mutation2(Mutation):
     def __init__(self, probability):
         super().__init__(probability)
 
     def mutate(self, ind: IntVectorIndividual) -> None:
-        # RETODO
-        pass
+        num_genes = len(ind.genome)
+        cut1 = GeneticAlgorithm.rand.randint(0, num_genes-1)
+        cut2 = cut1
+
+        while (cut2==cut1):
+            cut2 = GeneticAlgorithm.rand.randint(0, num_genes-1)
+
+        ind.genome[cut1], ind.genome[cut2] = ind.genome[cut2], ind.genome[cut1]
 
     def __str__(self):
         return "Mutation 2 (" + f'{self.probability}' + ")"
--- relatoriorascunho.md
+++ relatoriorascunho.md
@@ -9,3 +9,4 @@ Extras:
 aquilo de abrir o mais recente
 fast search do pairs
 meter o search mais limpo e legivel(quando da parse do ambiente vindo do ficheiro)
+a insert mutation que os profs nos deram era na verdade a insert mutation, por isso corrigi a class insert mutation para ter o algoritmo da insert mutation, e meti o algoritmo do invert mutation no mutation3
